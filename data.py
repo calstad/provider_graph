d8c7c62fa603e72b1b05e45ea09afd9ea040c013
Colin Alstad <colin.alstad@gmail.com>
2013-08-15 17:47:07 -0400

    Continue work on trying to get batch imports working.

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ class BatchGraphImporter:
         self.batch = neo4j.WriteBatch(self.db)
         for record in records:
             self.create_provider_subgraph(record)
-        # return self.batch.submit()
+        return self.batch.submit()
         
     def current_batch_index(self):
         batch_length = len(self.batch.requests)
@@ -20,15 +20,18 @@ class BatchGraphImporter:
 
     def create_provider_subgraph(self, record):
         self.create_provider_node(record)
-        # self.create_npi_node(record)
-        # self.create_name_nodes(record)
+        self.create_npi_node(record)
+        self.create_name_nodes(record)
+        self.create_address_nodes(record)
 
     def create_provider_node(self, record):
-        self.batch.get_or_create_indexed_node('providers', 'npi', record['npi']['npi'], record['provider'])
+        self.batch.create(node(record['provider']))
         self.current_provider_index = self.current_batch_index()
+        self.batch.add_indexed_node_or_fail('providers', 'npi', record['npi']['npi'], self.current_provider_index)
 
     def create_npi_node(self, record):
-        self.batch.get_or_create_indexed_node('npis', 'npi', record['npi']['npi'], record['npi'])
+        npi = record['npi']['npi']
+        npi_node = self.batch.create(record['npi'])
         self.batch.create(rel(self.current_provider_index, 'has_npi', self.current_batch_index()))
     
     def create_name_nodes(self, record):
@@ -38,19 +41,18 @@ class BatchGraphImporter:
             self.batch.create(rel(self.current_provider_index, 'has_name', self.current_batch_index()))
 
     def create_address_nodes(self, record):
-        addreses = record['addresses']
+        addresses = record['addresses']
         for address in addresses:
             self.batch.create(address)
             address_index = self.current_batch_index()
             self.batch.create(rel(self.current_provider_index, 'has_address', address_index))
-            self.create_zipcode_node(address_index, address)
-
-    def add_state_to_node(node_index, rel_name, state):
-        state_validator = re.compile("^[A-Z]{2}$")
-        if state_validator.match(state):
-            self.batch.get_or_create_indexed_node('states', 'name', state, {'name' : state})
-            state_node_index = self.increment_batch_index()
-            self.relate_nodes(node_index, rel, state_node_index)
-            
+            self.create_zipcode_node(address_index, address['zipcode'])
+    
+    def create_zipcode_node(self, address_index, zipcode):
+        if zipcode:
+            self.batch.get_or_create_indexed_node('zipcodes', 'zipcode', zipcode, {'zipcode' : zipcode})
+            zipcode_index = self.current_batch_index()
+            self.batch.get_or_create_indexed_relationship('addresses', 'zipcode', zipcode, address_index, 'in_zipcode', zipcode_index)
+                
 bgi = BatchGraphImporter('http://localhost:7474/db/data/')
 
