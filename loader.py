377ff118a779c4477e3c97579d6371637a63ae8d
Colin Alstad <colin.alstad@gmail.com>
2013-08-16 10:52:08 -0400

    Have loader take actual command line arguments for npi file and neo4j url.

--- loader.py
+++ loader.py
@@ -1,21 +1,22 @@
 import sys
 import csv
-from data import ProviderGraph
+from data import BatchGraphImporter
 from parser import RowParser
 
 def load_npi_file(file_name, neo4j_url, skip_header=True):
     with open(file_name) as npi_csv:
         npi_reader = csv.reader(npi_csv)
-        pg = ProviderGraph('http://localhost:7474/db/data/')
         if skip_header:
             npi_reader.next()
+        npi_importer = BatchGraphImporter(neo4j_url)
         rows = []
         for row in npi_reader:
             rp = RowParser(row).parse()
             rows.append(rp)
-            if len(rows) % 10000 == 0:
-                pg.create_provider_nodes(rows)
+            if len(rows) % 3000 == 0:
+                npi_importer.batch_persist(rows)
                 rows = []
+        npi_importer.batch_persist(rows)
 
 if __name__ == '__main__':
-    load_npi_file(sys.argv[1], "foo")
+    load_npi_file(sys.argv[1], sys.argv[2])
